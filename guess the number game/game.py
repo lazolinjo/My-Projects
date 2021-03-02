247e4c5775978aaa046e6583cb33dc9d73cad948
Lazar <lazko2002@gmail.com>
2021-03-02 21:22:35 +0100

    fixed

--- guess the number game/game.py
+++ guess the number game/game.py
@@ -9,10 +9,10 @@ print('Well, ' + name + ', I am thinking of a number between 1 and 20.')
 secretNumber = random.randint(1, 20)
 
 #if's 
-for guessesTaken in range(1, 7):    #Gives 7 guesses total
+for guessesTaken in range(1, 7):
     print('Take a guess.')
     guess = int(input())
-s
+
     if guess < secretNumber:
         print('Your guess is too low.')
     elif guess > secretNumber:
