582eb4ce2894bdb33f2bfb8b604ae4e8414764b8
Lazar <lazko2002@gmail.com>
2021-03-06 18:37:32 +0100

    Update mapit.py

--- mapit/mapit.py
+++ mapit/mapit.py
@@ -1,10 +1,10 @@
 import webbrowser, sys, pyperclip
 
-sys.argv # ['mapit.py', '870', 'Valencia', 'St.']
+sys.argv
 
 # Check if command line arguments were passed
 if len(sys.argv) > 1:
-    #['mapit.py', '870', 'Valencia', 'St.'] -> '870 Valencia St.'
+    
     address = ' '.join(sys.argv[1:])
 else:
     address = pyperclip.paste()
