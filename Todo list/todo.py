53868dba5c2cfef7e3d278f4a8841631d4d183c1
Lazar <lazko2002@gmail.com>
2021-03-09 13:08:06 +0100

    Made my first gui project

---
+++ Todo list/todo.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+
+todo = []
+
+layout = [
+    [sg.Text('ToDo')],
+    [sg.InputText('Enter Todo Item', key='todoKey'), sg.Button(button_text='Add', key="add_save")],
+    [sg.Listbox(values=todo, size=(40, 10), key="items"), sg.Button('Delete'), sg.Button('Edit')],
+]
+
+window = sg.Window('todo app', layout)
+while True:  # event loop
+    event, values = window.Read()
+    if event == "add_save":
+        todo.append(values['todoKey'])
+        window.FindElement('items').Update(values=todo)
+        window.FindElement('add_save').Update("Add")
+    elif event == "Delete":
+        todo.remove(values["items"][0])
+        window.FindElement('items').Update(values=todo)
+    elif event == "Edit":
+        edit_val = values["items"][0]
+        todo.remove(values["items"][0])
+        window.FindElement('items').Update(values=todo)
+        window.FindElement('todoKey').Update(values=edit_val)
+        window.FindElement('add_save').Update("Save")
+    elif event == None:
+        break
+
+window.Close()
