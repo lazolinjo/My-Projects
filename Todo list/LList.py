acabb341326a7b5871bfb9a9c971bf0357af7635
Lazar <lazko2002@gmail.com>
2022-02-21 22:43:28 +0100

    Bugfixes
    Fixed app crash on edit button press.

---
+++ Todo list/LList.py
@@ -0,0 +1,47 @@
+from asyncio import tasks
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import InputText
+
+def focus_control(event):
+    if window['todo_item'].get() == '':
+        window['todo_item'].update(value=defaultText)
+    window['todo_item'].update(select=True)
+    # window.refresh()
+    return None
+
+
+defaultText = "Enter your text here"
+tasks = []
+
+layout = [
+    [sg.Text('ToDo')],
+    [sg.InputText('Enter ToDo Item', key='todo_item'), sg.Button(button_text='Add', key="add_save")],
+    [sg.Listbox(values=tasks, size=(40, 10), key="items"), sg.Button('Delete'), sg.Button('Edit')],
+]
+
+window = sg.Window('LList', layout, icon=r'C:\Users\Andricka\Desktop\Projects\My-Projects\Todo list\logo.ico', finalize=True)
+window['todo_item'].update(select=True)
+window['todo_item'].Widget.bind("<FocusIn>", focus_control)
+
+while True:  # Event Loop
+    event, values = window.Read()
+    if event == "add_save":
+        tasks.append(values['todo_item'])
+        window.FindElement('items').Update(values=tasks)
+        window.FindElement('add_save').Update("Add")
+
+    elif event == "Delete":
+        tasks.remove(values["items"][0])
+        window.FindElement('items').Update(values=tasks)
+
+    elif event == "Edit":
+        edit_val = values["items"][0]
+        tasks.remove(values["items"][0])
+        window.FindElement('items').Update(values=tasks)
+        window.FindElement('todo_item').Update(value=edit_val)
+        window.FindElement('add_save').Update("Save")
+
+    elif event == None:
+        break
+
+window.Close()
